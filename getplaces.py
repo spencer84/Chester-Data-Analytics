ba29aecaebcadcb5c1a6e711b30754800a241940
spencer84 <36893802+spencer84@users.noreply.github.com>
2021-10-20 22:50:13 +0100

    Working to paginate the postcode query to run through all results. Not quite there yet.

--- getplaces.py
+++ getplaces.py
@@ -1,12 +1,33 @@
 import requests
 import json
 
-### Need to get OS Maps API Key
-### Store in git ignore file?
+
+### Retrieve OS Maps API Key
+### Use .gitignore to hide JSON file
 
 with open('API Key.json') as f:
     api_key = json.loads(f.read())
 key = api_key[0]['api_key']
 url = 'https://api.os.uk/search/places/v1/postcode'
 
+# Define a function to query the OS Places API and paginate through all results
+
+def postcode_query(url, key, postcode):
+    response = requests.get(url, params={'key': key, 'postcode': postcode})
+    return response.json()
+
+def full_query(url, key, postcode):
+    full_results = {}  # Initialise a dictionary to to store results through pagination
+    count = 0
+    response = postcode_query(url, key, postcode)
+    total_results = response.json()['header']['totalresults']  # Identify total results of query
+    while count < total_results:
+        for i in range(0,100):
+            full_results[i] = response[i]['DPA']
+            count += 1
+        response = postcode_query(url, key, postcode)
+
 response = requests.get(url,params ={'key':key,'postcode':'CH1'})
+
+
+response['totalresults']
