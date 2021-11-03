56051d0e4cdcda927691517dd33bd682db7c473f
spencer84 <36893802+spencer84@users.noreply.github.com>
2021-11-03 22:13:09 +0000

    Working to set up API for retrieving data from EPC. Still working through the authentification process.

--- getepcdata.py
+++ getepcdata.py
@@ -1,9 +1,14 @@
 import requests
 import json
+import base64
 
 ### Retrieve OS Maps API Key
 ### Use .gitignore to hide JSON file
 with open('API Key.json') as f:
     api_key = json.loads(f.read())
-key = api_key[0]['epc']
-url = 'https://api.os.uk/search/places/v1/postcode'  # URL for the Places API
+key = api_key[0]['epc_key']
+username = api_key[0]['epc_username']
+url = 'https://epc.opendatacommunities.org/api/v1/domestic/search'  # URL for the Places API
+encoded_api_key = base64.b64encode(bytes(username + ':' + key, 'utf-8'))
+
+response = requests.get(url, params={}, headers={ 'Authorization' : 'Basic %s' % encoded_api_key,'Accept':'application/json'})
