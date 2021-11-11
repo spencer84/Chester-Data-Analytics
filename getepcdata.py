407bfa61a0472b02b46a8b14cde9b2a25a9298d1
spencer84 <36893802+spencer84@users.noreply.github.com>
2021-11-11 22:52:47 +0000

    Wrote the get_postcode_data function to write results of postcode query to CSV.

--- getepcdata.py
+++ getepcdata.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import base64
+import csv
 
 ### Retrieve OS Maps API Key
 ### Use .gitignore to hide JSON file
@@ -9,8 +10,26 @@ with open('API Key.json') as f:
 key = api_key[0]['epc_key']
 username = api_key[0]['epc_username']
 url = 'https://epc.opendatacommunities.org/api/v1/domestic/search'  # URL for the Places API
+
 # Need to encode the username and key then strip off the extra bits
 encoded_api_key = str(base64.b64encode(bytes(username + ':' + key, 'utf-8')))[1:].replace('\'',"")
 #encoded_api_key = 'c2FtOTVAdnQuZWR1OjkzMzUyZDQ2N2I5YjU3YmZkNTNmZjBkMGQ1NzFlNjVhMjUyOWZiMzc='
 #,'Accept':'application/json'
-response = requests.get(url, params={}, headers={ 'Authorization' : 'Basic %s' % encoded_api_key})
+
+# Select postcode to search by
+postcode = 'CH1'
+
+def get_postcode_data(key, postcode):
+    """" Writes a CSV containing all results for the given postcode to the local directory
+    :param key: Encoded secret key for the OS API
+    :param postcode: Any UK Postcode or the first part (district portion)
+    :return: None
+    """
+    response = requests.get(url, params={'postcode':postcode}, headers={ 'Authorization' : 'Basic %s' % key, "Accept":'text/csv'})
+    csvfile = open(postcode + '.csv', 'w')
+    writer = csv.writer(csvfile, delimiter=' ')
+    for row in response.text.split('\n'):
+        writer.writerow(row)
+
+
+
