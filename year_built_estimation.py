946e86cb2168b3b497a4f9f87c4cd9de6c16e30d
spencer84 <36893802+spencer84@users.noreply.github.com>
2021-12-15 23:18:00 +0000

    First attempt at setting up the classifier model.
    Need to return predictions and probabilities to the dataframe.

--- year_built_estimation.py
+++ year_built_estimation.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import sklearn
+from sklearn import neighbors
 
 # Identify postcode
 
 postcode = 'CH1'
 
-### Import epc data
+# Import epc data
 epc =  pd.read_csv('epc_'+postcode+'.csv')
 
-### Import places data
+# Import places data
 places =  pd.read_csv('places_'+postcode+'.csv')
 
 epc.head()
@@ -19,7 +19,7 @@ df = pd.merge(places, epc, how = 'left', left_on = 'UPRN', right_on = 'uprn')
 
 df['construction-age-band'].value_counts(dropna = False)
 
-### Need to clean the 'construction-age-band' field
+# Need to clean the 'construction-age-band' field
 
 # Remove null values
 df.dropna(axis= 0,subset=['construction-age-band'], inplace= True)
@@ -30,11 +30,25 @@ variables_to_remove = ['NO DATA!','INVALID!']
 
 df = df[~df['construction-age-band'].isin(variables_to_remove)]
 
-#### KNN classifier
+# KNN classifier
 
-
-
-## Need to determine optimal value for K
-
+# Need to determine optimal value for K
 # Create a list to log error rates of
 k_results = []
+
+for k in range(1,15):
+
+    # Create a training/testing set
+
+    training_df = df.sample(n=int(len(df) / 2))
+
+    test_df = df[~df['uprn'].isin(training_df['uprn'])]
+
+
+    clf = neighbors.KNeighborsClassifier(k, weights="distance")
+
+    clf.fit(training_df[['X_COORDINATE','Y_COORDINATE']],training_df['construction-age-band'])
+
+    k_results.append(clf.score(test_df[['X_COORDINATE','Y_COORDINATE']],test_df['construction-age-band']))
+
+print(k_results)
