2b680b194159a2f4b9458d739da37dfe7a4833e8
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-02-21 23:17:33 +0000

    Added postcode in query

--- getpricepaid.py
+++ getpricepaid.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 url = 'https://landregistry.data.gov.uk/data/ppi/transaction-record.json?'
-properties = 'transactionId,transactionDate,pricePaid,propertyAddress.paon,propertyAddress.street'
+properties = 'transactionId,transactionDate,pricePaid,propertyAddress.paon,propertyAddress.street, propertyAddress.postcode'
 
 town = 'CHESTER'
 params_lr = {'_pageSize':200,
@@ -14,13 +14,13 @@ def price_paid_query(params_lr):
     return response
 
 ## Retrive data
-data = price_paid_query(params_lr)
+#data = price_paid_query(params_lr)
 
 ## Parse data to get a dataframe of the results
-data = data.json()['result']['items']
+#data = data.json()['result']['items']
 
 ## Convert json to dataframe
-df = pd.json_normalize(data)
+#df = pd.json_normalize(data)
 
 ## Recursively paginate through results
 
@@ -42,7 +42,7 @@ def get_full_price_paid(df, page, params_lr):
 df = get_full_price_paid(df, page, params_lr)
 
 ## Change column names
-col_names = ['url', 'Price Paid','Transaction Date', 'Transaction Id', 'Type', 'About','PAON', 'Street Name']
+col_names = ['url', 'Price Paid','Transaction Date', 'Transaction Id', 'Type', 'About','PAON', 'Street Name', 'Postcode']
 df.columns = col_names
 
 ## Output
@@ -51,3 +51,5 @@ df.to_csv('price_paid_'+town+'.csv')
 ## Paginating through the results for an entire town works, but is slow
 ## Is there a way to find out if a postcode contains a district?
 
+# Need to do some processing to identify a given postcode
+df['postcode district'] = df['Postcode'].apply(lambda x: x[:3])
