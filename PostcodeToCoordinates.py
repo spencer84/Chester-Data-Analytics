76b4a9c7cb3a8bd9592a152e81b90cce53a99399
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-03-05 16:35:58 +0000

    Added functionality to handle >100 postcodes

--- PostcodeToCoordinates.py
+++ PostcodeToCoordinates.py
@@ -1,7 +1,8 @@
 import requests
 
 def PostcodeGeocode(postcodes):
-    """" This is to return coordinates for a series of postcodes
+    """" This is to return coordinates for a series of postcodes.
+    Each request can only handle 100 postcodes, so this will paginate through until complete.
     :param postcodes: An array of postcodes to be geocoded
     :return: A zipped array of lat/long coordinates
     """
@@ -9,12 +10,25 @@ def PostcodeGeocode(postcodes):
     long = []
     endpoint = 'http://api.postcodes.io/postcodes/'
     # Send the request
-    results = requests.post(endpoint, {"postcodes":postcodes}).json()
-    # Parse results
-    for i in results['result']:
-        lat.append(i['result']['latitude'])
-        long.append(i['result']['longitude'])
-    return zip(lat, long)
+    if len(postcodes)>100:
+        start = 0
+        end = 100
+        batch = postcodes[start:end]
+        while start < len(postcodes):
+            results = requests.post(endpoint, {"postcodes": postcodes}).json()
+            # Parse results
+            for i in results['result']:
+                lat.append(i['result']['latitude'])
+                long.append(i['result']['longitude'])
+            start += 100
+            end += 100
+    else:
+        results = requests.post(endpoint, {"postcodes":postcodes}).json()
+        # Parse results
+        for i in results['result']:
+            lat.append(i['result']['latitude'])
+            long.append(i['result']['longitude'])
+        return zip(lat, long)
 
 
 
