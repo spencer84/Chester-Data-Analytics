d01770c2929041e43a5a8833e29b8aec3435edbe
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-03-19 18:58:00 +0000

    Adding sqlite3 db instead of csv files

--- getepcdata.py
+++ getepcdata.py
@@ -3,6 +3,7 @@ import json
 import base64
 import pandas as pd
 import sqlite3
+import datetime
 
 # Retrieve OS Maps API Key
 # Use .gitignore to hide JSON file
@@ -14,10 +15,6 @@ url = 'https://epc.opendatacommunities.org/api/v1/domestic/search'  # URL for th
 
 # Need to encode the username and key then strip off the extra bits
 encoded_api_key = str(base64.b64encode(bytes(username + ':' + key, 'utf-8')))[1:].replace('\'', "")
-
-
-# encoded_api_key = 'c2FtOTVAdnQuZWR1OjkzMzUyZDQ2N2I5YjU3YmZkNTNmZjBkMGQ1NzFlNjVhMjUyOWZiMzc='
-# 'Accept':'application/json'
 
 
 def get_postcode_data(key, postcode):
@@ -59,36 +56,47 @@ cur = con.cursor()
 
 
 # Initialise the table if not already done
-cur.execute('''CREATE TABLE IF NOT EXISTS epc
-               (lmk_key text, address1 text, address2 text, address3 text,
-                building_reference_number text, current_energy_rating text,	potential_energy_rating text,
-                current_energy_efficiency real,	potential_energy_efficiency real,
-                property_type text,	built_form text, inspection_date text,	local_authority	constituency text,
-                county text, lodgement_date text, transaction_type text, environment_impact_current real,
-                environment_impact_potential real,	energy_consumption_current real, energy_consumption_potential real,
-                co2_emissions_current real,	co2_emiss_curr_per_floor_area real,	co2_emissions_potential real, 
-                lighting_cost_current real,	lighting_cost_potential real, heating_cost_current real, 
-                heating_cost_potential real, hot_water_cost_current	real, hot_water_cost_potential real,
-                total_floor_area real,	energy_tariff text,	mains_gas_flag text, floor_level text,
-                flat_top_storey text, flat_storey_count int, main_heating_controls text,
-                multi_glaze_proportion real, glazed_type text,	glazed_area	extension_count int,
-                number_habitable_rooms int,	number_heated_rooms int,low_energy_lighting	int,
-                number_open_fireplaces int,	hotwater_description text,	hot_water_energy_eff text,
-                hot_water_env_eff text,	floor_description text,	floor_energy_eff text,	floor_env_eff text,
-                windows_description text,	windows_energy_eff text, windows_env_eff text, walls_description text,
-                walls_energy_eff text,	walls_env_eff text,	secondheat_description text, sheating_energy_eff text,
-                sheating_env_eff text,	roof_description text,	roof_energy_eff text,	roof_env_eff text,
-                mainheat_description text,	mainheat_energy_eff text, mainheat_env_eff text, 
-                mainheatcont_description text,	mainheatc_energy_eff text,	mainheatc_env_eff text,
-                lighting_description text,	lighting_energy_eff	text, lighting_env_eff text, main_fuel text,
-                wind_turbine_count int,	heat_loss_corridor text, unheated_corridor_length real,	floor_height real,
-                photo_supply text, solar_water_heating_flag text, mechanical_ventilation text,
-                address	local_authority_label text,	constituency_label text, posttown text,
-                construction_age_band text,	lodgement_datetime text, tenure	 text, fixed_lighting_outlets_count int,
-                low_energy_fixed_light_count int, uprn text, uprn_source text)''')
+# Let's start with a smaller one with a few key values
+# Query date is reflective of the date the data is requested
+cur.execute('''CREATE TABLE IF NOT EXISTS epc 
+            (address text,postcode text, current_energy_rating text, total_floor_area real, query_date text
+            )''')
+curr_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+for index, row in results.iterrows():
+    cur.execute("INSERT INTO epc VALUES(?,?,?,?,?)",(row.address,row['postcode'],row['current-energy-rating'],row['total-floor-area'],curr_time))
+cur.execute("SELECT * FROM epc")
+rows = cur.fetchall()
+cur.execute('''DROP TABLE epc''')
+# cur.execute('''CREATE TABLE IF NOT EXISTS epc
+#                (lmk_key text, address1 text, address2 text, address3 text,
+#                 building_reference_number text, current_energy_rating text,	potential_energy_rating text,
+#                 current_energy_efficiency real,	potential_energy_efficiency real,
+#                 property_type text,	built_form text, inspection_date text,	local_authority	constituency text,
+#                 county text, lodgement_date text, transaction_type text, environment_impact_current real,
+#                 environment_impact_potential real,	energy_consumption_current real, energy_consumption_potential real,
+#                 co2_emissions_current real,	co2_emiss_curr_per_floor_area real,	co2_emissions_potential real,
+#                 lighting_cost_current real,	lighting_cost_potential real, heating_cost_current real,
+#                 heating_cost_potential real, hot_water_cost_current	real, hot_water_cost_potential real,
+#                 total_floor_area real,	energy_tariff text,	mains_gas_flag text, floor_level text,
+#                 flat_top_storey text, flat_storey_count int, main_heating_controls text,
+#                 multi_glaze_proportion real, glazed_type text,	glazed_area	extension_count int,
+#                 number_habitable_rooms int,	number_heated_rooms int,low_energy_lighting	int,
+#                 number_open_fireplaces int,	hotwater_description text,	hot_water_energy_eff text,
+#                 hot_water_env_eff text,	floor_description text,	floor_energy_eff text,	floor_env_eff text,
+#                 windows_description text,	windows_energy_eff text, windows_env_eff text, walls_description text,
+#                 walls_energy_eff text,	walls_env_eff text,	secondheat_description text, sheating_energy_eff text,
+#                 sheating_env_eff text,	roof_description text,	roof_energy_eff text,	roof_env_eff text,
+#                 mainheat_description text,	mainheat_energy_eff text, mainheat_env_eff text,
+#                 mainheatcont_description text,	mainheatc_energy_eff text,	mainheatc_env_eff text,
+#                 lighting_description text,	lighting_energy_eff	text, lighting_env_eff text, main_fuel text,
+#                 wind_turbine_count int,	heat_loss_corridor text, unheated_corridor_length real,	floor_height real,
+#                 photo_supply text, solar_water_heating_flag text, mechanical_ventilation text,
+#                 address text,	local_authority_label text,	constituency_label text, posttown text,
+#                 construction_age_band text,	lodgement_datetime text, tenure	 text, fixed_lighting_outlets_count int,
+#                 low_energy_fixed_light_count int, uprn text, uprn_source text)''')
 
 # Insert a row of data
-cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
 
 # Save (commit) the changes
 con.commit()
@@ -96,4 +104,4 @@ con.commit()
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 con.close()
-results.to_csv('epc_' +postcode + '.csv')
+#results.to_csv('epc_' +postcode + '.csv')
