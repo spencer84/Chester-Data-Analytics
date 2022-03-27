dd8d5ebab3c15562aa5a1d4fd17b911265184e1d
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-03-27 13:11:01 +0100

    Improving documentation/formatting

--- db_init.py
+++ db_init.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-#
-def create_db(db_name = 'cda.db'):
+
+def create_db(db_name='cda.db'):
     """
     Create a db file - If a database doesn't already exists, the connect() function will create one in local dir
     :param db_name: Filename of the database, default name is 'cda.db'
@@ -9,30 +9,33 @@ def create_db(db_name = 'cda.db'):
     """
     con = sqlite3.connect(db_name)
     cur = con.cursor()
-    def create_epc(cur):
+
+    def create_epc(cursor):
         """
         Create a table for EPC records
-        :param cur:
+        :param cursor: sqlite3 cursor object from db connection
         :return:
         """
-        cur.execute('''CREATE TABLE IF NOT EXISTS epc 
-                    (address text, address1 text ,uprn text,postcode text, current_energy_rating text, total_floor_area real,
-                    lodgement_datetime text, query_date text
+        cursor.execute('''CREATE TABLE IF NOT EXISTS epc 
+                    (address text, address1 text ,uprn text,postcode text, current_energy_rating text, 
+                    total_floor_area real, lodgement_datetime text, query_date text
                     )''')
         return
 
-    def create_dlog(cur):
+    def create_dlog(cursor):
         """
         Create a log for when other tables are updated and which postcodes are included
-        :param cur:
+        :param cursor: sqlite3 cursor object from db connection
         :return:
         """
-        cur.execute("""CREATE TABLE IF NOT EXISTS data_log (postcode_district text, epc bool, 
+        cursor.execute("""CREATE TABLE IF NOT EXISTS data_log (postcode_district text, epc bool, 
         epc_date text, land_reg bool, land_reg_date text)""")
         return
+
     create_epc(cur)
     create_dlog(cur)
     con.close()
 
+
 if __name__ == "__main__":
-    create_db(db_name= 'test1.db')
+    create_db()
