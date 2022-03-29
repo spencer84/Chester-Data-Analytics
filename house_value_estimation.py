debece9e56219cf41ca44d0fc245d7b214c85dea
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-03-29 21:19:11 +0100

    Checking age of epc/land_reg data

--- house_value_estimation.py
+++ house_value_estimation.py
@@ -1,5 +1,7 @@
 import getepcdata as epc
+import getpricepaid as land
 import sqlite3
+import datetime
 
 # Define path for API keys JSON file
 path = 'API Key.json'
@@ -12,6 +14,7 @@ class Property:
         self.full_address = None
         self.db = 'cda.db'
         self.postcode_district = None
+        self.town = None
     def get_input(self):
         """
         Get data from user
@@ -22,17 +25,48 @@ class Property:
         # Remove spaces from postcode and sterilize to avoid SQL injection
         number = input("House/Flat Number:")
         self.number = number
-        self.postcode_district = self.postcode[:2]
+        self.postcode_district = self.postcode.split(" ")[0]
+        town = input("Town:")
+        self.town = town
     def check_postcode_data(self):
         # Connect to db
         con = sqlite3.connect(self.db)
         cur = con.cursor()
         cur.execute("SELECT * FROM data_log WHERE postcode_district =? ", (self.postcode_district,))
         results = cur.fetchall()
-        # Is there data from both the EPC and Land Registry within the last Month?
-        if len(results) == 0:
+        # Find most recent EPC records
+        cur.execute("""SELECT MAX(date) FROM (SELECT * FROM data_log WHERE postcode_district = 'CH1' 
+        AND data_table = 'epc')""")
+        max_epc = cur.fetchall()
+        if len(max_epc) == 0:
             print("No data exists for this postcode district. Getting EPC Data...")
             epc.get_postcode_epc_data(epc.get_key(path), self.postcode_district)
+        else:
+            epc_age = datetime.datetime.today() - datetime.datetime.fromisoformat(max_epc[0][0])
+            if epc_age > 30:
+                update_epc = input("EPC Data is more than 30 days old. Update? y/n")
+                if update_epc == 'y':
+                    epc.get_postcode_epc_data(epc.get_key(path), self.postcode_district)
+                elif update_epc == 'n':
+                    pass
+                else:
+                    print("Input not understood...Data will not be updated.")
+
+        # Find most recent land_reg records
+        params_lr = {'_pageSize': 200,
+                     '_view': 'basic',
+                     '_properties': properties,
+                     'propertyAddress.town': self.town}
+        cur.execute("""SELECT MAX(date) FROM (SELECT * FROM data_log WHERE postcode_district = 'CH1' 
+        AND data_table = 'land_reg')""")
+        max_land_reg = cur.fetchall()
+        if len(max_land_reg) == 0:
+            print("No data exists for this postcode district. Getting EPC Data...")
+            land.get_full_price_paid(epc.get_key(path), self.postcode_district)
+    def return_cursor(self):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        return cur
 
 
 
@@ -42,12 +76,15 @@ prop.postcode = 'CH1 1SD'
 prop.postcode_district = 'CH1'
 #prop.get_input()
 prop.check_postcode_data()
+cur = prop.return_cursor()
+
+
 
 
 ## Quickly create a new table to log data sources and track when updated
-test = 'CH2'
-import sqlite3
-con = sqlite3.connect('cda.db')
-cur = con.cursor()
-
-results  = cur.execute("SELECT * FROM data_log WHERE postcode_district =?",(test,))
+# test = 'CH2'
+# import sqlite3
+# con = sqlite3.connect('cda.db')
+# cur = con.cursor()
+#
+# results  = cur.execute("SELECT * FROM data_log WHERE postcode_district =?",(test,))
