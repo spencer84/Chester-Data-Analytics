cd4526d513963fdfe38b342d9465fd870e263b95
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-04-18 18:54:10 +0100

    Re-building land-registry query as an object to improve process

--- getpricepaid.py
+++ getpricepaid.py
@@ -2,6 +2,64 @@ import requests
 import datetime
 import sqlite3
 
+class LandData:
+    def __init__(self):
+        self.url = url = 'https://landregistry.data.gov.uk/data/ppi/transaction-record.json?'
+        self.propoerties = 'transactionId,transactionDate,pricePaid,propertyAddress.' \
+                           'paon,propertyAddress.street,propertyAddress.postcode'
+        self.town = None
+        self.page = 0
+        self.params = {'_pageSize': 200,
+             '_view': 'basic',
+             '_properties': properties,
+             'propertyAddress.town': self.town,
+             '_page':self.page}
+        self.status = None
+        self.conn = None
+        self.cur = None
+        self.results_to_add = []
+        self.request_status_code = None
+    def create_connection(self, db):
+        self.conn = sqlite3.connect(db)
+        print("Connected to "+db)
+    def create_cursor(self):
+        self.cur = self.conn.cursor()
+    def close_connection(self):
+        self.conn.commit()
+        self.conn.close()
+        print("Connection closed")
+
+
+    def price_paid_query(self,url, params_lr):
+        """
+        Queries the Land Registry API and with the
+        :param cur: DB Cursor Object
+        :param params_lr: Parameters for querying the Land Registry API
+        :return: Length of results
+        """
+        response = requests.get(self.url, params=self.params)
+        self.request_status_code = response.status_code
+        results = response.json()['result']['items']
+        # Need to write these results to the database
+        curr_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # Iterate through the results dataframe, adding each value to the DB
+        for i in results:
+            vals_to_insert = [params_lr['propertyAddress.town'], None, None, None, None, i['transactionDate']
+                , i['pricePaid'], curr_time]
+            property_details = i['propertyAddress']
+            # KeyError thrown if a given key isn't returned; Need to check before assigning value
+            if 'postcode' in property_details:
+                vals_to_insert[1] = get_postcode_district(i['propertyAddress']['postcode'])
+                vals_to_insert[2] = i['propertyAddress']['postcode']
+            if 'paon' in property_details:
+                vals_to_insert[3] = i['propertyAddress']['paon']
+            if 'street' in property_details:
+                vals_to_insert[4] = i['propertyAddress']['street']
+
+            self.results_to_add.append(vals_to_insert)
+
+
+
 
 def get_postcode_district(postcode):
     """ Returns the postcode district/area from a given postcode
@@ -24,7 +82,7 @@ params_lr = {'_pageSize': 200,
              'propertyAddress.town': town}
 
 
-def price_paid_query(cur, params_lr):
+def price_paid_query(cur,url, params_lr):
     """
     Queries the Land Registry API and with the
     :param cur: DB Cursor Object
@@ -36,6 +94,7 @@ def price_paid_query(cur, params_lr):
     # Need to write these results to the database
     curr_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # Iterate through the results dataframe, adding each value to the DB
+    results_to_add = []
     for i in results:
         vals_to_insert = [params_lr['propertyAddress.town'], None, None, None, None, i['transactionDate']
             , i['pricePaid'], curr_time]
@@ -49,9 +108,10 @@ def price_paid_query(cur, params_lr):
         if 'street' in property_details:
             vals_to_insert[4] = i['propertyAddress']['street']
 
-        cur.execute("INSERT INTO land_reg VALUES(?,?,?,?,?,?,?,?)"
-                    , tuple(vals_to_insert))
-    return len(results)
+        results_to_add.append(vals_to_insert)
+    return results_to_add
+
+get_request_status(params_lr, )
 
 ## Recursively paginate through results
 
