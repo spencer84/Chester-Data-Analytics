98cbd0bddab9d40d7a2246645edef47b614d8d58
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-04-18 21:35:33 +0100

    Re-building land-registry query as an object to improve process

--- getpricepaid.py
+++ getpricepaid.py
@@ -3,29 +3,34 @@ import datetime
 import sqlite3
 import queue
 
+
 class LandData:
     def __init__(self):
-        self.url = url = 'https://landregistry.data.gov.uk/data/ppi/transaction-record.json?'
+        self.url = 'https://landregistry.data.gov.uk/data/ppi/transaction-record.json?'
         self.propoerties = 'transactionId,transactionDate,pricePaid,propertyAddress.' \
                            'paon,propertyAddress.street,propertyAddress.postcode'
         self.town = None
         self.page = 0
         self.results_len = 0
-        self.params = {'_pageSize': 200,
-             '_view': 'basic',
-             '_properties': properties,
-             'propertyAddress.town': self.town,
-             '_page':self.page}
+        self.params = {'_view': 'basic',
+                       '_properties': properties,
+                       '_pageSize': 200,
+                       'propertyAddress.town': self.town,
+                       '_page': self.page}
         self.status = None
         self.conn = None
         self.cur = None
         self.results_to_add = queue.Queue()
         self.request_status_code = None
+        self.query_in_progress = False
+
     def create_connection(self, db):
         self.conn = sqlite3.connect(db)
-        print("Connected to "+db)
+        print("Connected to " + db)
+
     def create_cursor(self):
         self.cur = self.conn.cursor()
+
     def close_connection(self):
         self.conn.commit()
         self.conn.close()
@@ -33,10 +38,8 @@ class LandData:
 
     def price_paid_query(self):
         """
-        Queries the Land Registry API and with the
-        :param cur: DB Cursor Object
-        :param params_lr: Parameters for querying the Land Registry API
-        :return: Length of results
+        Queries the Land Registry API and with the given 'town' attribute of the object. Updates the request_status_code
+        and the results_len attributes
         """
         response = requests.get(self.url, params=self.params)
         self.request_status_code = response.status_code
@@ -57,6 +60,7 @@ class LandData:
                 vals_to_insert[3] = i['propertyAddress']['paon']
             if 'street' in property_details:
                 vals_to_insert[4] = i['propertyAddress']['street']
+            # Add each line of results to the queue of values to be added to the database
             self.results_to_add.put(vals_to_insert)
 
     def get_full_price_paid(self):
@@ -64,22 +68,18 @@ class LandData:
         while not end_of_results:  # Recursion until a page length less than max achieved
             self.price_paid_query()
             self.page += 1
-            if self.results_len <200:
+            if self.results_len < 200:
                 end_of_results = True
             print(self.page)
 
     def data_to_db(self):
-        while not self.results_to_add.empty():
+        while self.query_in_progress:
             self.cur.execute("")
         curr_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         cur.execute("INSERT INTO data_log VALUES(?,?,?)", (params_lr['propertyAddress.town'], 'land_reg', curr_time))
         con.commit()
         con.close()
         return
-
-
-
-
 
 
 def get_postcode_district(postcode):
@@ -103,7 +103,7 @@ params_lr = {'_pageSize': 200,
              'propertyAddress.town': town}
 
 
-def price_paid_query(cur,url, params_lr):
+def price_paid_query(cur, url, params_lr):
     """
     Queries the Land Registry API and with the
     :param cur: DB Cursor Object
@@ -131,6 +131,7 @@ def price_paid_query(cur,url, params_lr):
 
         results_to_add.append(vals_to_insert)
     return results_to_add
+
 
 ## Recursively paginate through results
 
