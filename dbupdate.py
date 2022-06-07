cefd107eadb2bd79bc1059c7cdcf91bbdeefa765
Sam Spencer <sam95@vt.edu>
2022-06-07 23:16:39 +0100

    Creating merged table

--- dbupdate.py
+++ dbupdate.py
@@ -1,3 +1,17 @@
 import sqlite3
+import getepcdata
+import getpricepaid
+
+conn = sqlite3.connect('cda.db')
+cur = conn.cursor()
+
+# Create a merged table in the CDA database
+
+cur.execute("""CREATE TABLE merged AS select * from land_reg
+         inner join epc 
+         on land_reg.postcode = epc.postcode 
+         and land_reg.PAON like '%' || epc.address1|| '%'""")
 
 # Program to update the database and perform all data transactions
+def update_(conn, postcode_area):
+    """Update the land registry table with records for a given postcode"""
