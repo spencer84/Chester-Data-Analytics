6136ce7cf8fca05de1244c77f233a189f0af28fd
Sam Spencer <sam95@vt.edu>
2022-06-23 21:18:23 +0100

    Identifying counts of unique addresses from each table to ensure best merge

--- dbupdate.py
+++ dbupdate.py
@@ -12,10 +12,34 @@ cur.execute("""CREATE TABLE merged AS select distinct * from land_reg
          on land_reg.postcode = epc.postcode 
          and land_reg.PAON like '%' || epc.address1|| '%'""")
 
+# How many unique combinations are being captured?
+
+cur.execute("select count(distinct address) from (select PAON || ' '|| postcode AS address from land_reg)")
+
+land_reg_count = cur.fetchall()
+
+cur.execute("select count(postcode) from merged")
+
+merged_count = cur.fetchall()
+
 # How to handle duplicate records (same property, different transactions)?
 # Getting multiple land reg records, but not distinct because of timestamp
 # Need to improve the initial upload process?
 # Delete older transactions
+
+# Sort transactions by age then delete
+cur.execute("""SELECT
+	*,
+	RANK () OVER ( 
+		PARTITION BY postcode + PAON
+		ORDER BY transaction_date DESC
+	) transaction_rank 
+FROM
+	merged
+""")
+
+ranked = cur.fetchall()
+
 
 cur.execute("""DELETE FROM merged
 WHERE transaction_date < (
