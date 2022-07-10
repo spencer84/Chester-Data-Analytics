9049f31d2b45d506170120619823ca1f975483cc
Sam Spencer <sam95@vt.edu>
2022-07-10 22:04:17 +0100

    Created functions to update both EPC and Land Registry records. Then update the merged table by deleting existing tables and re-creating it.

--- dbupdate.py
+++ dbupdate.py
@@ -1,18 +1,18 @@
 import sqlite3
 import getepcdata as epc
 import datetime
-import getpricepaid as land_data
+import getpricepaid as land
 
 conn = sqlite3.connect('cda.db')
 cur = conn.cursor()
 
 
-def update_all(postcode_area):
+def update_epc(cur,postcode_area, max_days = 7):
     """
-    Update the data from both EPC records and
-    :param postcode_area: A given postcode area to update. All records with a postcode starting with this figure will
-    be updated.
-    :return:
+    Update the EPC records for all postcodes in a given postcode area.
+    :param postcode_area: The postcode area (prefix) to update
+    :param max_days: Length of time in days before the data is updated.
+    :return: None
     """
     # Update EPC records
     cur.execute("""SELECT MAX(date) FROM (SELECT * FROM data_log WHERE postcode_district = :postcode  
@@ -23,14 +23,22 @@ def update_all(postcode_area):
         epc.get_postcode_epc_data(epc.get_key(path), postcode_area)
     else:
         epc_age = datetime.datetime.today() - datetime.datetime.fromisoformat(max_epc[0][0])
-        if epc_age.days >= 7:
-            print("EPC data is 1 week old. Updating records...")
+        if epc_age.days >= max_days:
+            print("EPC data is more than 1 week old. Updating records...")
             epc.get_postcode_epc_data(epc.get_key(path), postcode_area)
             print("EPC data updated.")
 
-    # Update Land Registry Records
+def update_land(cur, town, max_days = 7):
+    """
+    Update the Land Registry records for all properties in a given town.
+    :param town: The town to update records for.
+    :param max_days: Length of time in days before the data is updated.
+    :return: None
+    """
+    land_data = land.LandData()
+    land_data.town = town
     cur.execute("""SELECT MAX(date) FROM (SELECT * FROM data_log WHERE postcode_district = :postcode
-            AND data_table = 'land_reg')""", {"postcode": self.postcode_district})
+            AND data_table = 'land_reg')""", {"town": town})
     max_land_reg = cur.fetchall()
     print(str(max_land_reg))
     if max_land_reg[0] == (None,):
@@ -38,21 +46,23 @@ def update_all(postcode_area):
         land_data.get_full_price_paid()
     else:
         land_age = datetime.datetime.today() - datetime.datetime.fromisoformat(max_land_reg[0][0])
-        if land_age.days >= 7:
+        if land_age >= max_days:
+            print("Land Registry data is more than 1 week old. Updating records...")
             land_data.get_full_price_paid()
-
-
+            print("Land Registry data updated.")
 
 
 # Create a merged table in the CDA database
+def create_merged_table(cur):
+    cur.execute("""DROP TABLE IF EXISTS recent_land;
+    CREATE TABLE recent_land as 
+    select *, max(transaction_date) as most_recent_transaction from land_reg group by paon, postcode""")
 
-cur.execute("""CREATE TABLE recent_land as 
-select *, max(transaction_date) as most_recent_transaction from land_reg group by paon, postcode""")
-
-cur.execute("""CREATE TABLE merged AS select * from recent_land
-         inner join epc 
-         on recent_land.postcode = epc.postcode 
-         and recent_land.PAON like '%' || epc.address1|| '%'""")
+    cur.execute("""DROP TABLE IF EXISTS merged;
+    CREATE TABLE merged AS select * from recent_land
+             inner join epc 
+             on recent_land.postcode = epc.postcode 
+             and recent_land.PAON like '%' || epc.address1|| '%'""")
 
 
 # Need to take only most recent record from land reg data
