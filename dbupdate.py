e731243aa9a5974a93b347b36b87a97948a895e1
Sam Spencer <sam95@vt.edu>
2022-07-11 20:23:16 +0100

    Setting up the script to update all three components (epc, landreg, and the merged table) for given areas.

--- dbupdate.py
+++ dbupdate.py
@@ -7,7 +7,6 @@ import getpricepaid as land
 path = 'API Key.json'
 
 conn = sqlite3.connect('cda.db')
-cur = conn.cursor()
 
 def update_epc(cur,postcode_area, max_days = 7):
     """
@@ -66,6 +65,15 @@ def create_merged_table(cur):
              on recent_land.postcode = epc.postcode 
              and recent_land.PAON like '%' || epc.address1|| '%'""")
 
+    # Delete older transactions
+
+    cur.execute("""DELETE FROM merged
+    WHERE transaction_date < (
+    SELECT MAX(transaction_date) FROM merged t2 WHERE merged.postcode = t2.postcode
+    AND merged.PAON = t2.PAON
+    ) 
+    """)
+
     # # *** Engineer additional features ***
     # # Calculate time since the original transaction
     # self.merged_table['transaction_date'] = pd.to_datetime(self.merged_table['transaction_date'])
@@ -82,43 +90,28 @@ def create_merged_table(cur):
 
 # How many unique combinations are being captured?
 
-cur.execute("""select count(distinct address) from (select PAON || ' '|| postcode AS address from land_reg)""")
-
-cur.execute("""select count(address) as address_count, address, transaction_date from (select PAON || ' '|| postcode AS address
-            , transaction_date from land_reg) group by address order by address_count desc""")
-
-land_reg_count = cur.fetchall()
-
-cur.execute("select count(postcode) from merged")
-
-merged_count = cur.fetchall()
+# cur.execute("""select count(distinct address) from (select PAON || ' '|| postcode AS address from land_reg)""")
+#
+# cur.execute("""select count(address) as address_count, address, transaction_date from (select PAON || ' '|| postcode AS address
+#             , transaction_date from land_reg) group by address order by address_count desc""")
+#
+# land_reg_count = cur.fetchall()
+#
+# cur.execute("select count(postcode) from merged")
+#
+# merged_count = cur.fetchall()
 
 # How to handle duplicate records (same property, different transactions)?
 # Getting multiple land reg records, but not distinct because of timestamp
 # Need to improve the initial upload process?
-# Delete older transactions
 
-# Sort transactions by age then delete
-cur.execute("""SELECT
-	*,
-	RANK () OVER ( 
-		PARTITION BY postcode + PAON
-		ORDER BY transaction_date DESC
-	) transaction_rank 
-FROM
-	merged
-""")
-
-ranked = cur.fetchall()
-
-
-cur.execute("""DELETE FROM merged
-WHERE transaction_date < (
-SELECT MAX(transaction_date) FROM merged t2 WHERE merged.postcode = t2.postcode
-AND merged.PAON = t2.PAON
-) 
-""")
 
 # Program to update the database and perform all data transactions
-def update_(conn, postcode_area):
+def update_db(conn, postcode_area, town):
     """Update the land registry table with records for a given postcode"""
+    # Create a cursor from the database connection
+    cur = conn.cursor()
+    # Update EPC
+    update_epc()
+    update_land()
+    create_merged_table()
