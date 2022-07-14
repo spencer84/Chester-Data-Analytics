f303c93cbdab90ec67a889b12b0799467d4f5c78
Sam Spencer <sam95@vt.edu>
2022-07-14 22:39:13 +0100

    Median function not supported in Sqlite3; Using Average until I switch to MySQL

--- dbupdate.py
+++ dbupdate.py
@@ -84,7 +84,7 @@ def create_postcode_district_features(cur):
     :param cur: Cursor object for database
     """
     cur.execute("""
-    SELECT postcode, median(total_floor_area) as median_floor_area from epc GROUP BY postcode;
+    SELECT postcode, average(total_floor_area) as avg_floor_area from epc GROUP BY postcode;
     """)
 
 
