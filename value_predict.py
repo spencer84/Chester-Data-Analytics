d5e17e4abfebb4b567a6a7fff104b07e386d7678
spencer84 <36893802+spencer84@users.noreply.github.com>
2022-07-18 23:26:06 +0100

    Setting up Flask app for receiving input.

--- value_predict.py
+++ value_predict.py
@@ -1,8 +1,16 @@
-from flask import Flask, redirect
-import house_value_estimation
+from flask import Flask, redirect, request, url_for
+#import house_value_estimation
 app = Flask(__name__)
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict.html', methods = ['GET','POST'])
 def predict():
-    if request.methods = 'POST':
+    if request.method == 'POST':
+        print(request)
+    else:
+        return redirect(url_for('/predict.html'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
