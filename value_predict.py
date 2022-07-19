b94f99d36643235193c4f1a9b21a61da588afa32
Sam Spencer <sam95@vt.edu>
2022-07-19 22:01:22 +0100

    Adding templates to expand on basic app functionality

--- value_predict.py
+++ value_predict.py
@@ -1,16 +1,25 @@
-from flask import Flask, redirect, request, url_for
-#import house_value_estimation
+from flask import Flask, redirect, request, url_for, render_template
+
+# import house_value_estimation
 app = Flask(__name__)
 
-@app.route('/predict.html', methods = ['GET','POST'])
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        print(request)
+        postcode = request.form['Postcode']
+        return """<p> postcode </p>"""
     else:
-        return redirect(url_for('/predict.html'))
+        return render_template('predict.html')
+
+
+# @app.route('/result.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
