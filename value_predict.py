67053bb4905d1eb553dd70f346d4a350f59c8265
Sam Spencer <sam95@vt.edu>
2022-07-21 23:24:49 +0100

    Added town feature from Post; Setting up prediction model

--- value_predict.py
+++ value_predict.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, request, url_for, render_template
-
+from house_value_estimation  import Property
 # import house_value_estimation
 app = Flask(__name__)
 
@@ -11,10 +11,24 @@ def home():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    """Predict the value of a given property using the inputed values."""
     if request.method == 'POST':
         postcode = request.form.get('postcode')
         paon = request.form.get('paon')
-        return f"""<p> {postcode} </p> <"""
+        town = request.form.get('town')
+        prop = Property()
+        prop.postcode = postcode
+        prop.paon = paon
+        prop.town = town
+        # Calculate value
+        if prop.validate_postcode():
+            prop.check_for_model()
+            prop.check_features()
+            value = prop.predict()
+        else:
+            return """<p> Postcode not valid! </p> <a <href='/predict'> Try again </a>. """
+
+        return f"""<p> The predicted value of {paon} {postcode} is £{value}</p> <"""
     elif request.method == 'GET':
         return render_template('predict.html')
 
